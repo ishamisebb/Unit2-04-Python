fc44d44b767f0a26e344f4900fdf297c6d816765
Ishami Sebgoya <ishami.sebgoya@stu.ocsb.ca>
2023-10-26 01:53:47 +0000

    Added pizza_cost.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Created by: Ms Raffin
+# Created on: May 1st, 2021
+# This program contains constants.
+HST = 0.13
+LABOUR_COST = 2.00
+RENTAL_COST = 2.25
+INGRED_COST = 1.5
